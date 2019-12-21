ff90e6fd7948e81cb9714f854a3569ea43e53c91
Ankur-1980 <58996699+Ankur-1980@users.noreply.github.com>
2019-12-21 13:07:21 -0500

    Add files via upload
    ??? Determines correct vacation destination (even if not outputted correctly)
    ??? Determines correct travel method (allowed off-by-one errors, even if not
    outputted correctly)

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
-//vacation type
 var vacationType = prompt("What kind of trip would you like to go on?", "musical, tropical, or adventurous");
 
-//where to
 if (vacationType === 'musical') {
 		var loCal = "New Orleans";
 } else if (vacationType === 'tropical') {
-		var loCal = "a beach vacation in Mexico";
+		var loCal = "a beach resort in Mexico";
 } else if (vacationType === 'adventurous') {
-		var loCal = "whitewater rafting the Grand Canyon";
+		var loCal = "go whitewater rafting the Grand Canyon";
 }
 
-//group Size
 var groupSize = prompt("How many are in your group?", "Enter a number");
 
-//tranporation
 if (groupSize <=2 ) {
 		var transPo = "a first class flight";
 } else if ((groupSize >= 3) && (groupSize < 6)) {
@@ -21,8 +17,6 @@ if (groupSize <=2 ) {
 }	else if (groupSize >= 6) {
 		var transPo = "a charter flight";
 }
-
-// final output and recommendation
 
 if ( transPo && loCal) {
 	var txt;
