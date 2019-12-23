685394e23980deaab67f1bef4ff41a0c657f8ab5
Ankur-1980 <58996699+Ankur-1980@users.noreply.github.com>
2019-12-22 20:02:23 -0500

    Add files via upload
    Almost there.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,37 @@
-var vacationType = prompt("What kind of trip would you like to go on?", "musical, tropical, or adventurous");
+var txt;
+var loCal;
+var transPo;
 
-if (vacationType === 'musical') {
-		var loCal = "New Orleans";
-} else if (vacationType === 'tropical') {
-		var loCal = "a beach resort in Mexico";
-} else if (vacationType === 'adventurous') {
-		var loCal = "go whitewater rafting the Grand Canyon";
-}
+function myFunction() {
 
-var groupSize = prompt("How many are in your group?", "Enter a number");
+	var vacationType = prompt("What kind of trip would you like to go on?", "musical, tropical, or adventurous");
+		console.log(vacationType);
 
-if (groupSize <=2 ) {
-		var transPo = "a first class flight";
-} else if ((groupSize >= 3) && (groupSize < 6)) {
-		var transPo = "a helicopter";
-}	else if (groupSize >= 6) {
-		var transPo = "a charter flight";
-}
+	if (vacationType == null) || vacationType == "") {
+		alert("Please choose the type of vacation you would like to go on")
+	} else if (vacationType === 'musical') {
+			var loCal = "New Orleans";
+	} else if (vacationType === 'tropical') {
+			var loCal = "a beach resort in Mexico";
+	} else if (vacationType === 'adventurous') {
+			var loCal = "go whitewater rafting the Grand Canyon";
+	}
 
-if ( transPo && loCal) {
-	var txt;
-	var result = "Since you are a group of "+ groupSize +" going on a "+ vacationType +" you should take "+ transPo +" to "+ loCal +".";
-	console.log(result);
-}
-	{ txt = "Since you are a group of "+ groupSize +" going on a "+ vacationType +" vacation, you should take "+ transPo +" to "+ loCal +".";
-	document.getElementById("here").innerHTML = txt;
+	var groupSize = prompt("How many are in your group?", "Enter a number");
+		console.log(groupSize);
+
+	if ((groupSize > 0) && (groupSize <= 2)) {
+			var transPo = "a first class flight";
+	} else if ((groupSize >= 3) && (groupSize < 6)) {
+			var transPo = "a helicopter";
+	}	else if (groupSize >= 6) {
+			var transPo = "a charter flight";
+	}
+
+	if (transPo && loCal) {
+		var txt;
+		var result = "Since you are a group of "+ groupSize +" going on a "+ vacationType +" you should take "+ transPo +" to "+ loCal +".";
+		alert(result);
+		console.log(result);
+	}
 }
